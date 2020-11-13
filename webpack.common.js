903c9a12de6cbd69de5474d972bbb2d19268b15c
ArdKr <mkras01@gmx.de>
2020-11-13 16:24:17 +0100

    Add assets

--- webpack.common.js
+++ webpack.common.js
@@ -14,6 +14,28 @@ module.exports = {
         test: /\.html$/,
         use: "html-loader",
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            publicPath: "imgs/",
+            outputPath: "imgs",
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            publicPath: "fonts/",
+            outputPath: "fonts",
+          },
+        },
+      },
     ],
   },
 };
