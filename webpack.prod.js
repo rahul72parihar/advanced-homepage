470fd8759d494f0f98712b256a64afdaa35e9353
ArdKr <mkras01@gmx.de>
2020-11-14 13:29:47 +0100

    Add postcss

--- webpack.prod.js
+++ webpack.prod.js
@@ -14,7 +14,12 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.js$/,
