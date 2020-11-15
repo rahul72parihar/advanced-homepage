3a1beeffae439698dafe2d122f15a93e4bcafc51
ArdKr <mkras01@gmx.de>
2020-11-15 14:55:05 +0100

    Add Navbar & Hero

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./index.scss";
+
+const hamburgerMenuButton = document.querySelector(".hamburger");
+const headerElement = document.querySelector("header");
+
+hamburgerMenuButton.addEventListener("click", () => {
+  headerElement.classList.toggle("open");
+});
