17955fd02d633f09057c84e8072f8833b1ccbc0f
Rahul Parihar <rahul.parihar@jalantechnologies.com>
2023-12-14 13:25:55 +0530

    remove all files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./index.scss";
-
-const hamburgerMenuButton = document.querySelector(".hamburger");
-const headerElement = document.querySelector("header");
-
-hamburgerMenuButton.addEventListener("click", () => {
-  headerElement.classList.toggle("open");
-});
